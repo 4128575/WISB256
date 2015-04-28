d8c61bc6779a7e453ebf587d099089ea07f8479b
a4128575 <richard.sad@hotmail.com>
2015-04-28 13:51:43 +0000

    uitwerking van opdrachten week2

--- Werkcollege/week1.py
+++ Werkcollege/week1.py
@@ -1,1 +1,28 @@
-print('lol')
+import urllib.request
+def check_fermat(a, b, c, n):
+    if n>2 and a**n+b**n==c**n:
+        print('Hole smokes, Fermat was wrong!')
+    else:
+        print('No, that does not work.')
+def inputfermat():
+    x=int(input('What is a? '))
+    y=int(input('What is b? '))
+    z=int(input('What is c? '))
+    m=int(input('What is n? '))
+    check_fermat(x,y,z,m)
+
+zipcode = '02492'
+url = 'http://uszip.com/zip/' + zipcode
+conn2= urllib.request.urlopen(url)
+
+for line in conn2.fp:
+    line = line.strip()
+    lineStr = str(line, encoding='utf8')
+    if 'Total population' in lineStr:
+        print(lineStr)
+#    if 'Longitude' in line: 
+#        print(line)
+#    if 'Latitude' in line: 
+#        print(line)
+
+conn2.close()
