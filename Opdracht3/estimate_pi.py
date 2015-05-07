1fc34f86d48595a72f3d3f0c5167b32c600f838e
a4128575 <richard.sad@hotmail.com>
2015-05-07 19:38:50 +0000

    Derde inlever opgave af met bonus

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -1,7 +1,35 @@
 import random
+import math
+import sys
 
-def dropneedle(l):
-    x=random.random()
-    y=random.random()
-    print(x,y)
-dropneedle(1)
+if len(sys.argv)<=2 or len(sys.argv)>=5:
+    print('Use: estimate_pi.py N L')
+elif float(sys.argv[2])<=0:
+    print('AssertionError: L should be greater than 0')
+else:
+    N=int(sys.argv[1])
+    L=float(sys.argv[2])
+    if len(sys.argv)==4:
+        random.seed(sys.argv[3])
+        
+    def dropneedle(l):
+        y0=random.random()
+        theta=random.vonmisesvariate(0,0)
+        y1=y0+math.sin(theta)*l
+        if abs(math.ceil(y1)-math.ceil(y0))>0.01:
+            return True
+        else: 
+            return False
+    hits=0
+    
+    for k in range(N):
+        if dropneedle(L)==True:
+            hits+=1
+    
+    if float(sys.argv[2])<=1:
+        pi=2*L*N/(hits)
+    else:
+        pi=2*(math.asin(1/L)+math.sqrt(L**2-1)-L)/(1-hits/N)
+    
+    print(hits , 'hits in', N , 'tries')
+    print('Pi =', pi)
