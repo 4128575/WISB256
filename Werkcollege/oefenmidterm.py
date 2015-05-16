f55fd5c72a133ad2751b5a1b447efa21f1ba0b05
a4128575 <richard.sad@hotmail.com>
2015-05-16 09:31:26 +0000

    t/m derde oefenopgave af

--- Werkcollege/oefenmidterm.py
+++ Werkcollege/oefenmidterm.py
@@ -1,9 +1,40 @@
-n = int(input())
-for i in range(1,n+1):
-    invoer=input()
-    lijst=invoer.split()
-    if len(lijst)>4:
-        print('Crackers!')
-    else:
-        print(invoer+' krAh!')
-    
+import math
+
+#A-Lorre
+#n = int(input())
+#for i in range(1,n+1):
+#    invoer=input()
+#    lijst=invoer.split()
+#    if len(lijst)>4:
+#        print('Crackers!')
+#    else:
+#        print(invoer+' krAh!')
+
+#B-RPN
+#rpn = input()
+#lijst = rpn.split()
+#if lijst[2]=='+':
+#    a=int(lijst[0])+int(lijst[1])
+#    a ="{0:.3f}".format(a)
+#    print(a)
+#elif lijst[2]=='-':
+#    b=int(lijst[0])-int(lijst[1])
+#    b ="{0:.3f}".format(b)
+#    print(b)
+#elif lijst[2]=='*':
+#    c=int(lijst[0])*int(lijst[1])
+#    c ="{0:.3f}".format(c)
+#    print(c)
+#elif lijst[2]=='/':
+#    d = int(lijst[0])/int(lijst[1])
+#    d ="{0:.3f}".format(d)
+#    print(d)
+
+#C-Nerfgun
+vuurkracht = int(input())
+zwaartekracht = int(input())
+afstand = int(input())
+waarde=(afstand*zwaartekracht)/(vuurkracht*vuurkracht)
+hoek = (1/2)*math.asin(waarde)
+hoek ="{0:.2f}".format(hoek)
+print(hoek)
