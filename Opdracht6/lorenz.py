cfbfbf2128b3d002faa98101c0c3c2493f15ec13
a4128575 <richard.sad@hotmail.com>
2015-06-07 10:52:01 +0000

    inlever 6 (niet af) met bonus

--- Opdracht6/lorenz.py
+++ Opdracht6/lorenz.py
@@ -29,24 +29,3 @@ class Lorenz(object):
             return True
         else:
             return False
-
-L1=Lorenz([-1,1,0])
-L2=Lorenz([-1.001,1.001,0.001])
-u1=L1.solve(50,0.01)
-u2=L2.solve(50,0.01)
-#print(u1[0,0],u2[0,0])
-#print(u1[-1,0],u2[-1,0])
-
-sigma=10
-beta=8/3
-rho=1.01
-
-u1=math.sqrt(beta*(rho-1))
-L3=Lorenz([u1,u1,rho-1],sigma,rho,beta)
-aap=L3.isstable([u1,u1,rho-1])
-
-L4=Lorenz([0,0,0])
-aap2=L4.isstable([0,0,0])
-
-print(aap)
-print(aap2)
